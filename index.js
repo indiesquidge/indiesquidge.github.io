629e299503718fb4fe7ab031437a351a43776cdb
Austin Wood <3409645+indiesquidge@users.noreply.github.com>
2026-07-16 12:33:17 -0600

    Remove particles with setTimeout after they finish animating
    Following along: https://courses.joshwcomeau.com/wham/01-particles/01.01-cleanup
    
    We could have added an `animationend` event listener to the particle and
    remove them that way, but this has a few trade-offs:
    1. Josh says he's had issues with this not firing consistently
    2. If we used multiple keyframe animations it's not easy to figure out
       which animation just ended (many CSS libs will remove or garble the
       `animationName` property on the event)
    3. There is a perf issue with the number of parallel event listeners
       registered if we had hundreds of particles. This can be improved by
       lifting things to the parent element, but that adds more complexity
       and still doesn't fully address the perf concerns

--- index.js
+++ index.js
@@ -13,9 +13,12 @@ function range(start, end, step = 1) {
   return output;
 }
 
+const FADE_OUT_DURATION = 1000;
+
 const particleHeader = document.querySelector('.particleHeader');
 
 particleHeader.addEventListener('click', (event) => {
+  const particles = [];
 
   range(5).forEach(() => {
     const particle = document.createElement('span');
@@ -23,7 +26,16 @@ particleHeader.addEventListener('click', (event) => {
     particle.classList.add('particle');
     particle.style.left = Math.random() * 100 + '%';
     particle.style.top = Math.random() * 100 + '%';
+    particle.style.animationDuration = FADE_OUT_DURATION + 'ms';
 
     particleHeader.appendChild(particle);
+
+    particles.push(particle);
   });
+
+  setTimeout(() => {
+    particles.forEach((particle) => {
+      particle.remove();
+    });
+  }, FADE_OUT_DURATION + 200);
 });
